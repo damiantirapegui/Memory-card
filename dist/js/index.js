a70e176c5daccf5623ca7ac451314cee2081c65e
Jonathan Tirapegui <137382571+damiantirapegui@users.noreply.github.com>
2024-11-02 22:57:32 +0100

    Updated my code if card matches

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,9 +1,33 @@
 const fronts = document.querySelectorAll(".memory-card");
+let flippedCards = [];
 fronts.forEach((element) => {
     const card = element;
-    const cardValue = card.dataset.card;
     card.addEventListener("click", () => {
-        element.classList.toggle("flip");
-        console.log(cardValue);
+        if (card.classList.contains("flip") || flippedCards.length === 2)
+            return;
+        card.classList.add("flip");
+        flippedCards.push(card);
+        if (flippedCards.length === 2) {
+            const [card1, card2] = flippedCards;
+            const cardValue1 = card1.dataset.card;
+            const cardValue2 = card2.dataset.card;
+            if (cardValue1 === cardValue2) {
+                flippedCards = [];
+            }
+            else {
+                setTimeout(() => {
+                    card1.classList.remove("flip");
+                    card2.classList.remove("flip");
+                    flippedCards = [];
+                }, 1000);
+            }
+        }
     });
 });
+// If sats för att kontrollera att Arrayen innehåller 2 kort.
+// Desctructa båda korten som hamnar i arrayen.
+// Lägg ett värde på varsit kort. TIPS använd dataset.
+// Nestla if sats som kontrollerar att korten matchar.
+// Om dem matchar då ska du tömma arreyen för att lägga till 2 nya kort.
+// Annars om dem inte matchar då kan du ta bort (remove )card1 och card 2.
+// Glöm inte att tömma arrayen här också för att börja om
